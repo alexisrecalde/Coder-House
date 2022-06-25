54c4f59eae2e5d2f05af6cd9ed4153b2db1c196a
Alexis Recalde <alexis.recalde@hotmail.com>
2022-06-25 12:07:55 -0300

    agregando contador de carrito

--- js/main.js
+++ js/main.js
@@ -84,56 +84,7 @@ const productos = [
     img: "./assets/productos/compu4.jpg",
   },
 ];
-//prueba de recorrido
-for (const e of productos) {
-  console.log(e);
-}
-const carrito = [];
 
-function agregarAlCarrito(id) {
-  let producto = productos.find((producto) => producto.id == id);
-
-  let productoEnCarrito = carrito.find((producto) => producto.id == id);
-
-  console.log(producto);
-  console.log(productoEnCarrito);
-
-  //si el producto ya existe en el carrito suma la cantidad, sino lo agrega al array carrito
-  if (productoEnCarrito) {
-    productoEnCarrito.cantidad++;
-  } else {
-    carrito.push(producto);
-    producto.cantidad = 1;
-  }
-}
-
-agregarAlCarrito(01);
-agregarAlCarrito(01);
-agregarAlCarrito(02);
-agregarAlCarrito(01);
-agregarAlCarrito(03);
-agregarAlCarrito(03);
-console.log(carrito);
-
-function calcularTotal() {
-  let total = 0;
-  for (const e of carrito) {
-    let precioProducto = e.cantidad * e.precio;
-    total += precioProducto;
-  }
-  console.log(total);
-}
-
-calcularTotal();
-
-function EliminarProducto(id) {
-  carrito[id].cantidad--;
-  console.log(carrito[id].cantidad);
-}
-EliminarProducto(00);
-EliminarProducto(00);
-EliminarProducto(00);
-calcularTotal();
 /********************SLIDER**********************/
 
 const sliderImgArray = [
@@ -212,3 +163,54 @@ productos.forEach((item) => {
   console.log(div);
   productosContainer.append(div);
 });
+
+/********************CARRITO**********************/
+//prueba de recorrido
+for (const e of productos) {
+  console.log(e);
+}
+const carrito = [];
+
+function agregarAlCarrito(id) {
+  let producto = productos.find((producto) => producto.id == id);
+
+  let productoEnCarrito = carrito.find((producto) => producto.id == id);
+
+  console.log(producto);
+  console.log(productoEnCarrito);
+
+  //si el producto ya existe en el carrito suma la cantidad, sino lo agrega al array carrito
+  if (productoEnCarrito) {
+    productoEnCarrito.cantidad++;
+  } else {
+    carrito.push(producto);
+    producto.cantidad = 1;
+  }
+}
+
+agregarAlCarrito(01);
+agregarAlCarrito(01);
+agregarAlCarrito(02);
+agregarAlCarrito(01);
+agregarAlCarrito(03);
+agregarAlCarrito(03);
+console.log(carrito);
+
+function calcularTotal() {
+  let total = 0;
+  for (const e of carrito) {
+    let precioProducto = e.cantidad * e.precio;
+    total += precioProducto;
+  }
+  console.log(total);
+}
+
+calcularTotal();
+
+const botonAgregarAlCarrito = document.querySelector(".add-btn");
+const contdorCarritos = document.querySelector("#contador-carrito");
+const precioTotal = document.querySelector("#precio-total");""
+
+console.log(botonAgregarAlCarrito, contdorCarritos);
+
+botonAgregarAlCarrito.addEventListener("click", agregarAlCarrito);
